5ce2573fa3ee456b0f4d504935ac48ce0de28f6d
philipotieno <iamphilipotieno@gmail.com>
2021-05-10 19:03:35 +0300

    Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 my-14 lg:px-48 px-5">
+      <div className="lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center">
+        Sidebar
+      </div>
+      <div className="lg:col-span-9 bg-white rounded-2xl col-span-12">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
