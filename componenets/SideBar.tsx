a39ea5b6009ce1ddc67e9e51e08da0b4d9a957ad
philipotieno <iamphilipotieno@gmail.com>
2021-05-11 18:56:59 +0300

    Style buttons

--- componenets/SideBar.tsx
+++ componenets/SideBar.tsx
@@ -9,24 +9,24 @@ const SideBar = () => {
       <img
         src="https://philipotieno.github.io/photos/phill2.JPG"
         alt="Philip Otieno"
-        className="w-32 h-32 mx-auto rounded-full"
+        className="mx-auto w-32 h-32 rounded-full"
       />
       <h3 className="my-4 text-3xl font-medium tracking-wider">
         <span>Philip</span>
         Otieno
       </h3>
-      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">
+      <p className="my-3 px-2 py-1 bg-gray-200 rounded-full">
         Software Engineer
       </p>
       <a
-        className="px-2 py-1 my-3 bg-gray-200 rounded-full flex items-center justify-center"
+        className="flex items-center justify-center my-3 px-2 py-1 bg-gray-200 rounded-full"
         href=""
         download="name"
       >
         <GiTie className="w-6, h-6" /> Download Resume
       </a>
       {/* Social Icons */}
-      <div className="flex justify-around w-9/2 my-5 text-green-500 md:w-full mx-auto">
+      <div className="w-9/2 flex justify-around mx-auto my-5 text-green-500 md:w-full">
         <a href="">
           <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
@@ -35,16 +35,23 @@ const SideBar = () => {
         </a>
       </div>
       {/* Addresss */}
-      <div>
-        <div>
+      <div
+        className="my-5 py-4 bg-gray-200"
+        style={{ marginLeft: '-1rem', marginRight: '-1rem' }}
+      >
+        <div className="flex items-center justify-center space-x-2">
           <GoLocation />
           <span>Nairobi, Kenya</span>
         </div>
-        <p>iamphilipotieno@gmail.com</p>
+        <p className="my-2">iamphilipotieno@gmail.com</p>
       </div>
       {/* Email Button */}
-      <button>Email Me</button>
-      <button>Toggle Theme</button>
+      <button className="my-2 py-2 w-8/12 text-white bg-gradient-to-r rounded-full from-green-200 to-blue-400">
+        Email Me
+      </button>
+      <button className="my-2 py-2 w-8/12 text-white bg-gradient-to-r rounded-full from-green-200 to-blue-400">
+        Toggle Theme
+      </button>
     </div>
   );
 };
