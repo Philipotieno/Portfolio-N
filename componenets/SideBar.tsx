d1d801b1a2a4b3a91c460fe7f72846293c308748
philipotieno <iamphilipotieno@gmail.com>
2021-05-13 00:07:41 +0300

    Make site more resoonsive

--- componenets/SideBar.tsx
+++ componenets/SideBar.tsx
@@ -9,24 +9,24 @@ const SideBar = () => {
       <img
         src="https://philipotieno.github.io/photos/phill2.JPG"
         alt="Philip Otieno"
-        className="mx-auto w-32 h-32 rounded-full"
+        className="w-32 h-32 mx-auto rounded-full"
       />
-      <h3 className="font-kaushan my-4 text-3xl font-medium tracking-wider">
+      <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
         <span className="text-green">Philip</span>
         Otieno
       </h3>
-      <p className="my-3 px-2 py-1 bg-gray-200 rounded-full">
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">
         Software Engineer
       </p>
       <a
-        className="flex items-center justify-center my-3 px-2 py-1 bg-gray-200 rounded-full"
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
         href=""
         download="name"
       >
         <GiTie className="w-6, h-6" /> Download Resume
       </a>
       {/* Social Icons */}
-      <div className="w-9/2 text-green flex justify-around mx-auto my-5 md:w-full">
+      <div className="flex justify-around mx-auto my-5 w-9/2 text-green md:w-full">
         <a href="">
           <AiFillGithub className="w-8 h-8 cursor-pointer" />
         </a>
@@ -36,7 +36,7 @@ const SideBar = () => {
       </div>
       {/* Addresss */}
       <div
-        className="my-5 py-4 bg-gray-200"
+        className="py-4 my-5 bg-gray-200"
         style={{ marginLeft: '-1rem', marginRight: '-1rem' }}
       >
         <div className="flex items-center justify-center space-x-2">
@@ -46,12 +46,13 @@ const SideBar = () => {
         <p className="my-2">iamphilipotieno@gmail.com</p>
       </div>
       {/* Email Button */}
-      <button className="from-green my-2 py-2 w-8/12 text-white bg-gradient-to-r rounded-full to-blue-400" 
-      onClick={()=>window.open('mailto:iamphilipotieno@gmail.com')}
+      <button
+        className="w-8/12 py-2 my-2 text-white rounded-full bg-gradient-to-r focus:outline-none from-green to-blue-400"
+        onClick={() => window.open('mailto:iamphilipotieno@gmail.com')}
       >
         Email Me
       </button>
-      <button className="from-green my-2 py-2 w-8/12 text-white bg-gradient-to-r rounded-full to-blue-400">
+      <button className="w-8/12 py-2 my-2 text-white rounded-full bg-gradient-to-r focus:outline-none from-green to-blue-400">
         Toggle Theme
       </button>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,11 @@ import SideBar from '../componenets/SideBar';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-12 gap-6 my-14 lg:px-48 px-5">
-      <div className="lg:col-span-3 bg-white rounded-2xl col-span-12 p-4 text-center">
+    <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:px-48 sm:px-20 md:px-32">
+      <div className="col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl">
         <SideBar />
       </div>
-      <div className="lg:col-span-9 bg-white rounded-2xl col-span-12">
+      <div className="col-span-12 bg-white lg:col-span-9 rounded-2xl">
         <Component {...pageProps} />
       </div>
     </div>
