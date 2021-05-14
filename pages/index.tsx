4bc310686d88d295564ed3e4ba3934be715b3ca7
philipotieno <iamphilipotieno@gmail.com>
2021-05-14 17:21:26 +0300

    Get server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
+import { GetServerSideProps } from 'next';
 import { services } from '../data';
-const index = () => {
-  console.log(services);
-  
+const index = (services) => {
+  console.log('CLIENT', services);
+
   return <div>Project Page</div>;
 };
 
 export default index;
+
+export const getServerSideProps = async (context: GetServerSideProps) => {
+  const res = await fetch('http://localhost:3000/api/services');
+  const data = await res.json();
+
+  console.log('SERVER', services);
+
+  return {
+    props: {
+      services: data.services,
+    },
+  };
+};
