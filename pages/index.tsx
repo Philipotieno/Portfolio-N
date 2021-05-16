8416841f405d1f47f987a1a6ca9b2ac6081e5c2b
philipotieno <iamphilipotieno@gmail.com>
2021-05-16 23:46:34 +0300

    Add tailwind style

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import { services } from '../data';
 
 const index = () => {
   return (
-    <div>
-      <h5>
+    <div className="flex flex-col px-6 pt-1">
+      <h5 className="my-3 font-medium">
         Hi, I'm Philip Otieno, full stack engineer with experience in web
         development using Python(Django/Flask), Node and React😊. I found myself
         taking a new career path as a web developer with the possibility of
@@ -13,11 +13,16 @@ const index = () => {
         berries and sometimes I go to the gym 🏋! Follow me on social networks
         to be friends!
       </h5>
-      <div>
-        <h6>What I offer</h6>
-        <div>
+      <div
+        className="p-4 mt-5 bg-gray-400"
+        style={{ marginLeft: '-1.5rem', marginRight: '-1.5rem' }}
+      >
+        <h6 className="my-3 text-xl font-bold">What I offer</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
           {services.map((service) => (
-            <ServiceCard service={service} />
+            <div className="bg-gray-200 rounded-lg lg:col-span-1">
+              <ServiceCard service={service} />
+            </div>
           ))}
         </div>
       </div>
