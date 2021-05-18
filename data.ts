fbec9250ed7cc5c9257258a3a939269d68da0d68
philipotieno <iamphilipotieno@gmail.com>
2021-05-18 17:04:37 +0300

    feat: add data

--- data.ts
+++ data.ts
@@ -1,8 +1,9 @@
 import { RiComputerLine } from 'react-icons/ri';
-import { IService } from './type';
-import { FaServer } from "react-icons/fa";
-import { AiOutlineAntDesign, AiOutlineApi } from "react-icons/ai";
-import { MdDeveloperMode } from "react-icons/md";
+import { IService, ISkill } from './type';
+import { FaServer } from 'react-icons/fa';
+import { AiOutlineAntDesign, AiOutlineApi } from 'react-icons/ai';
+import { MdDeveloperMode } from 'react-icons/md';
+import { BsCircleFill } from 'react-icons/bs';
 
 export const services: IService[] = [
   {
@@ -13,37 +14,93 @@ export const services: IService[] = [
   },
   {
     Icon: RiComputerLine,
-    title: "Frontend Development",
+    title: 'Frontend Development',
     about:
-      "I can build a beautiful and scalable SPA using <b> HTML</b>,<b>CSS</b>   and <b>React.js</b> ",
+      'I can build a beautiful and scalable SPA using <b> HTML</b>,<b>CSS</b>   and <b>React.js</b> ',
   },
   {
     Icon: FaServer,
-    title: "Backend  Development",
+    title: 'Backend  Development',
     about:
-      "handle database, server, api using <b>Express </b> & other popular frameworks",
+      'handle database, server, api using <b>Express </b> & other popular frameworks',
   },
   {
     Icon: AiOutlineApi,
-    title: "API Development",
+    title: 'API Development',
     about:
-      "I can develop robust  REST API using <b>django-rest-api</b>  & <b>Node API</b> ",
+      'I can develop robust  REST API using <b>django-rest-api</b>  & <b>Node API</b> ',
   },
   {
     Icon: MdDeveloperMode,
-    title: "Competitive Coder",
-    about: "a daily problem solver in <b>HackerRank</b>  and <b>Leet Code</b> ",
+    title: 'Competitive Coder',
+    about: 'a daily problem solver in <b>HackerRank</b>  and <b>Leet Code</b> ',
   },
   {
     Icon: AiOutlineAntDesign,
-    title: "UI/UX designer",
+    title: 'UI/UX designer',
     about:
-      "stunning user interface designer using <b>Figma</b>  and  <b>Framer</b> ",
+      'stunning user interface designer using <b>Figma</b>  and  <b>Framer</b> ',
   },
   {
     Icon: RiComputerLine,
-    title: "Whatever",
+    title: 'Whatever',
     about:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis minima autem!",
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis minima autem!',
+  },
+];
+
+export const languages: ISkill[] = [
+  {
+    Icon: BsCircleFill,
+    name: 'Python',
+    level: '79%',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Java Script',
+    level: '60%',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Flutter',
+    level: '30$',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'React',
+    level: '70%',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Django',
+    level: '70%',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Bootstrap',
+    level: '70%',
+  },
+];
+
+export const tools: ISkill[] = [
+  {
+    Icon: BsCircleFill,
+    name: 'Figma',
+    level: '85',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Photoshop',
+    level: '45',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Illustrator',
+    level: '60',
+  },
+  {
+    Icon: BsCircleFill,
+    name: 'Framer',
+    level: '45',
   },
 ];
