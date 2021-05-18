f7944c02a8d84f4a0bd8aaf9d79b4e82266e8c2a
philipotieno <iamphilipotieno@gmail.com>
2021-05-18 17:14:39 +0300

    feat: add education and experience

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,5 +1,28 @@
 const resume = () => {
-  return <div className="p-4">Resume page</div>;
+  return (
+    <div className="px-4">
+      {/* Education & Exp */}
+      <div className="grid gap-6 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Education</h5>
+          <div>
+            <h5 className="my-2 text-xl font-bold">BSc With Edu</h5>
+            <p className="font-semibold">Karatina University(2012-2016)</p>
+            <p className="my-3">Relevant Courses Procedural Programming</p>
+          </div>
+        </div>
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Experience</h5>
+          <div>
+            <h5 className="my-2 text-xl font-bold">Andela Kenya</h5>
+            <p className="font-semibold">Software Engineer</p>
+            <p className="my-3">Relevant Courses Procedural Programming</p>
+          </div>
+        </div>
+      </div>
+      {/* Language & tools  */}
+    </div>
+  );
 };
 
 export default resume;
