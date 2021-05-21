9d36cbfe7f953bb9a506bd58d75272213567b597
philipotieno <iamphilipotieno@gmail.com>
2021-05-21 14:50:54 +0300

    feat: add interactivity function to the navbar

--- componenets/ProjectsNavbar.tsx
+++ componenets/ProjectsNavbar.tsx
@@ -1,23 +1,31 @@
 import { FunctionComponent } from 'react';
 import { Category } from '../type';
 
-export const NavItem: FunctionComponent<{ value: Category | 'all' }> = ({
-  value,
-}) => {
+export const NavItem: FunctionComponent<{
+  value: Category | 'all';
+  handleFilterCategory: Function;
+}> = ({ value, handleFilterCategory }) => {
   return (
-    <li className="capitalize cursor-pointer hover:text-green">{value}</li>
+    <li
+      className="capitalize cursor-pointer hover:text-green"
+      onClick={() => handleFilterCategory(value)}
+    >
+      {value}
+    </li>
   );
 };
 
-const ProjectsNavbar = () => {
+const ProjectsNavbar: FunctionComponent<{ handleFilterCategory: Function }> = (
+  props
+) => {
   return (
     <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
-      <NavItem value="all" />
-      <NavItem value="react" />
-      <NavItem value="node" />
-      <NavItem value="postgres" />
-      <NavItem value="express" />
-      <NavItem value="django" />
+      <NavItem value="all" {...props} />
+      <NavItem value="react" {...props} />
+      <NavItem value="node" {...props} />
+      <NavItem value="postgres" {...props} />
+      <NavItem value="express" {...props} />
+      <NavItem value="django" {...props} />
     </div>
   );
 };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,27 @@
-import { projects } from '../data';
+import { projects as projectsData } from '../data';
 import ProjectsCard from '../componenets/ProjectsCard';
 import ProjectsNavbar from '../componenets/ProjectsNavbar';
+import { useState } from 'react';
+import { Category } from '../type';
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState('all');
+
+  const handleFilterCategory = (category: Category | 'all') => {
+    if (category === 'all') {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+    const newArray = projectsData.filter((project) =>
+      project.category.includes(category)
+    );
+    setProjects(newArray);
+  };
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{ height: '65vh' }}>
-      <ProjectsNavbar />
+      <ProjectsNavbar handleFilterCategory={handleFilterCategory} />
       <div className="relative grid grid-cols-12 gap-4 my-3 bg-gray-200 rounded-lg dark:bg-dark-200">
         {projects.map((project) => (
           <div className="col-span-12 sm:col-span-6 lg:col-span-4">
