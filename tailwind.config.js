4123923a6659cab44c15c7daa1f76ebf84f7c5f0
philipotieno <iamphilipotieno@gmail.com>
2021-05-25 13:02:21 +0300

    feat: prepare for deployement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
       kaushan: ['Kaushan Script'],
     },
     boxShadow: {
       'custom-light': '0 0 10px #313131',
       'custom-dark': '5-x 5-x 10px #0a0c0e, -5px -5px 10px #14161c',
     },
+    
     extend: {
       colors: {
         green: {
