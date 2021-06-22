80ebd787291ff60be84d10c5f87915338e02b137
philipotieno <iamphilipotieno@gmail.com>
2021-06-22 10:20:09 +0300

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import SideBar from '../components/SideBar';
 import Navbar from '../components/Navbar';
 import { ThemeProvider } from 'next-themes';
 import { AnimatePresence } from 'framer-motion';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps, router }) {
+  const rout = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    rout.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      rout.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [rout.events]);
   return (
     <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 px-5 lg:px-36 xl:px-48 my-14 lg:mb-0 sm:px-20 md:px-32">
